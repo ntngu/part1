f6a64aca52d67b373e05073db4e899abe58ff809
Nhan Nguyen <nhantnguyen42@gmail.com>
2022-12-30 12:35:29 -0800

    Finished upto 1.14.

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 const App = () => {
+  const [selected, setSelected] = useState(0);
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -9,22 +10,41 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-
+  ];
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length));
+  const [max, setMax] = useState(0);
   const getRandomAnecdote = () => {
     const val = Math.floor(Math.random() * anecdotes.length);
-    console.log(val);
     setSelected(val);
+  };
+
+  const voteAnecdote = () => {
+    const temp = [...votes];
+    temp[selected] += 1
+    let max = -Infinity;
+    let position = 0;
+    for (let i = 0; i < anecdotes.length; i++) {
+      if (votes[i] > max) {
+        max = votes[i];
+        position = i
+      }
+    }
+    setMax(position);
+    setVotes(temp);
   }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}<br />
+      has {votes[selected]} votes<br />
+      <button onClick={ () => voteAnecdote() }>vote</button>
       <button onClick={ () => getRandomAnecdote() } >next anecdote</button>
+      <h1>Anecdote with the most votes</h1>
+      {anecdotes[max]}<br />
+      has {votes[max]}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
